f63f711ee818fa7e24aa07e67d1f7f9dfc7bdef4
theoJA <theophilus.ja@gmail.com>
2017-10-01 18:32:13 -0400

    test_getUserPassword success

--- src/server/logic/model/User.java
+++ src/server/logic/model/User.java
@@ -2,12 +2,27 @@ package server.logic.model;
 
 public class User {
 	int userId;
+	String password;
 	
-	public User(int userId) {
+	
+	public User(int userId, String password) {
+		this.setUserId(userId);
+		this.setUserPassword(password);
+	}
+	
+	// making the setter methods private so only the user class can set the id
+	private void setUserId(int userId) {
 		this.userId = userId;
+	}
+	private void setUserPassword(String password) {
+		this.password = password;
 	}
 	
 	public int getUserId() {
 		return userId;
+	}
+	
+	public String getUserPassword() {
+		return password;
 	}
 }
--- src/server/logic/model/tests/UserTest.java
+++ src/server/logic/model/tests/UserTest.java
@@ -7,11 +7,19 @@ import server.logic.model.User;
 
 public class UserTest {
 
-	private User testUser = new User(7); 
+	private User testUser = new User(7, "password"); 
+	
+	// All setters are implemented privately in the User class
+	// Only getters will be tested
 	
 	@Test
-	public void test_userId() {
+	public void test_getUserId() {
 		assertEquals(7, testUser.getUserId());
 	}
-
+	
+	@Test
+	public void test_getUserPassword() {
+		assertEquals("password", testUser.getUserPassword());
+	}
+	
 }
