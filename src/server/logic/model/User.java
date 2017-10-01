c5479b5becb4e3bde0a11900c88f21512ddb37a8
theoJA <theophilus.ja@gmail.com>
2017-10-01 17:37:17 -0400

    test_userId success

---
+++ src/server/logic/model/User.java
@@ -0,0 +1,13 @@
+package server.logic.model;
+
+public class User {
+	int userId;
+	
+	public User(int userId) {
+		this.userId = userId;
+	}
+	
+	public int getUserId() {
+		return userId;
+	}
+}
--- src/server/logic/model/tests/UserTest.java
+++ src/server/logic/model/tests/UserTest.java
@@ -3,12 +3,15 @@ package server.logic.model.tests;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import server.logic.model.User;
 
 public class UserTest {
 
+	private User testUser = new User(7); 
+	
 	@Test
-	public void TestUserInstance() {
-		assertEquals(1, 1);
+	public void test_userId() {
+		assertEquals(7, testUser.getUserId());
 	}
 
 }
