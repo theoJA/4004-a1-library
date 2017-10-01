e080eac9a3c993c31fa3a7370db56c47618700c7
theoJA <theophilus.ja@gmail.com>
2017-10-01 17:22:44 -0400

    TestUserInstance assertEquals function passed

---
+++ src/server/logic/model/tests/UserTest.java
@@ -0,0 +1,14 @@
+package server.logic.model.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserTest {
+
+	@Test
+	public void TestUserInstance() {
+		assertEquals(1, 1);
+	}
+
+}
