e1cb9dc93d3595b14c9478b2bb0da4002f09591b
theoJA <theophilus.ja@gmail.com>
2017-10-01 21:00:22 -0400

    test_getBookTitle success

--- src/server/logic/model/Title.java
+++ src/server/logic/model/Title.java
@@ -2,18 +2,26 @@ package server.logic.model;
 
 public class Title {
 	String ISBN;
+	String bookTitle;
 	
-	public Title(String ISBN) {
+	public Title(String ISBN, String bookTitle) {
 		this.setTitleISBN(ISBN);
+		this.setBookTitle(bookTitle);
 	}
 	
 	// Private setters for the Title class
 	private void setTitleISBN(String ISBN) {
 		this.ISBN = ISBN;
 	}
+	private void setBookTitle(String bookTitle) {
+		this.bookTitle = bookTitle;
+	}
 	//
 	
 	public String getTitleISBN() {
 		return ISBN;
+	}
+	public String getBookTitle() {
+		return bookTitle;
 	}
 }
--- src/server/logic/model/tests/TitleTest.java
+++ src/server/logic/model/tests/TitleTest.java
@@ -7,10 +7,17 @@ import server.logic.model.Title;
 
 public class TitleTest {
 
-	private Title testTitle = new Title("1234567890123");
+	private Title testTitle = new Title("1234567890123", "Hello World!");
 	
 	@Test
 	public void test_getTitleISBN() {
 		assertEquals("1234567890123", testTitle.getTitleISBN());
 	}
+	
+	@Test
+	public void test_getBookTitle() {
+		assertEquals("Hello World!", testTitle.getBookTitle());
+	}
+	
+	
 }
