078fee7d137de3651a544be92d8133af3256ab1e
theoJA <theophilus.ja@gmail.com>
2017-10-02 15:27:45 -0400

    Item class: test_getItemISBN SUCCESS

--- src/server/logic/model/Item.java
+++ src/server/logic/model/Item.java
@@ -2,18 +2,27 @@ package server.logic.model;
 
 public class Item {
 	int itemId;
+	String ISBN;
 	
-	public Item(int itemId) {
+	public Item(int itemId, String ISBN) {
 		this.setItemId(itemId);
+		this.setItemISBN(ISBN);
 	}
 	
 	// Private setters for Item class
 	private void setItemId(int itemId) {
 		this.itemId = itemId;
 	}	
+	private void setItemISBN(String ISBN) {
+		this.ISBN = ISBN;
+	}
 	//
 	
 	public int getItemId() {
 		return itemId;
+	}
+	
+	public String getItemISBN() {
+		return ISBN;
 	}
 }
--- src/server/logic/model/tests/ItemTest.java
+++ src/server/logic/model/tests/ItemTest.java
@@ -7,11 +7,19 @@ import server.logic.model.Item;
 
 public class ItemTest {
 
-	private Item testItem = new Item(1);
+	private Item testItem = new Item(1, "1234567890123");
+	
+	// Item setters automatically gets tested when instance of Item is created
+	// Getters tests both the getters and setters
 	
 	@Test
 	public void test_getItemId() {
 		assertEquals(1, testItem.getItemId());
+	}
+	
+	@Test
+	public void test_getItemISBN() {
+		assertEquals("1234567890123", testItem.getItemISBN());
 	}
 
 }
