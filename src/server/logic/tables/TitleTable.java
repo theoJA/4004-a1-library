938338d9a7684a71743c7be28862b88c83c5cf5e
theoJA <theophilus.ja@gmail.com>
2017-10-04 18:45:04 -0400

    CLASS: TitleTable, TEST: getTitleList SUCCESS

--- src/server/logic/tables/TitleTable.java
+++ src/server/logic/tables/TitleTable.java
@@ -16,14 +16,19 @@ public class TitleTable {
     private TitleTable(){
     	//set up the default list with some instances
     	String[] ISBNList=new String[]{"9781442668584","9781442616899","9781442667181","9781611687910","9781317594277"};
-    	String[] titlenameList=new String[]{"By the grace of God","Dante's lyric poetry ","Courtesy lost","Writing for justice","The act in context"};
+    	String[] titlenameList=new String[]{"By the grace of God","Dante's lyric poetry","Courtesy lost","Writing for justice","The act in context"};
     	for(int i=0;i<ISBNList.length;i++){
     		Title newTitle=new Title(ISBNList[i],titlenameList[i]);
     		titleList.add(newTitle);
 		}
     	//logger.info(String.format("Operation:Initialize TitleTable;TitleTable: %s", titleList));
-    };
+    }
+    
     public static final TitleTable getInstance() {
         return TitleListCollection.INSTANCE;
     }
+    
+    public List<Title> getTitleList() {
+		return titleList;
+	}
 }
--- src/server/logic/tables/tests/TitleTableTest.java
+++ src/server/logic/tables/tests/TitleTableTest.java
@@ -2,10 +2,16 @@ package server.logic.tables.tests;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
+
+import server.logic.model.Title;
 import server.logic.tables.TitleTable;
+import java.util.List;
 
 public class TitleTableTest {
 
+	private String testISBN = "9781442616899";
+	private String testBookTitle = "Dante's lyric poetry";
+	
 	// Assigning the address in memory of instance of TitleTable to a variable
 	private TitleTable testTitleTable = TitleTable.getInstance();
 	
@@ -14,5 +20,16 @@ public class TitleTableTest {
 		// Check if UserTable.getInstance() indeed returns the instance of UserTable
 		assertEquals(testTitleTable, TitleTable.getInstance());
 	}
+	
+	@Test
+	public void test_getTitleList() {
+		List<Title> testTitleList = testTitleTable.getTitleList();
+		
+		assertEquals(testTitleList, testTitleTable.getTitleList());
+		assertEquals(testISBN, testTitleTable.getTitleList().get(1).getISBN());
+		assertEquals(testBookTitle, testTitleTable.getTitleList().get(1).getBookTitle());
+	}
+	
+	
 
 }
