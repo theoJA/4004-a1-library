750641e43dbb0494a8b6b71eab907419f1392674
theoJA <theophilus.ja@gmail.com>
2017-10-10 15:43:03 -0400

    CLASS: InputHandler, TEST: processInput(all statements) SUCCESS

--- src/server/logic/handler/InputHandler.java
+++ src/server/logic/handler/InputHandler.java
@@ -7,6 +7,20 @@ public class InputHandler {
 	
 	public static final int WAITING = 0;
 	public static final int FINISHWAITING=1;
+	public static final int CLERK = 2;
+    public static final int USER = 3;
+    public static final int CREATEUSER=4;
+    public static final int CREATETITLE=5;
+    public static final int CREATEITEM=6;
+    public static final int DELETEUSER=7;
+    public static final int DELETETITLE=8;
+    public static final int DELETEITEM=9;
+    public static final int BORROW=10;
+    public static final int RENEW=11;
+    public static final int RETURN=12;
+    public static final int PAYFINE=13;
+    public static final int CLERKLOGIN=14;
+    public static final int USERLOGIN=15;
 
 	OutputHandler outputHandler=new OutputHandler();
 	
@@ -14,12 +28,306 @@ public class InputHandler {
 		 String output = "";
 		 Output o = new Output("",0);
 		 ServerOutput oo = new ServerOutput(output,o.getState());
-		 if (state == WAITING) {
+		 if (state == WAITING) {	// TESTED
 	        	output = "Who Are you?Clerk or User?";
 	            state = FINISHWAITING;
 	            oo.setOutput(output);
 	            oo.setState(state);
-	     }
+         }else if (state == FINISHWAITING) { // TESTED
+            if (input.equalsIgnoreCase("clerk")) {
+            	output="Please Input The Password:";
+            	state=CLERKLOGIN;
+                oo.setOutput(output);
+	            oo.setState(state);
+            }else if (input.equalsIgnoreCase("user")) { // TESTED
+            	output="Please Input Username and Password:'username,password'";
+            	state=USERLOGIN;
+                oo.setOutput(output);
+	            oo.setState(state);
+            }else{	// TESTED
+            	output = "Who Are you?Clerk or User?";
+            	state = FINISHWAITING;
+            	oo.setOutput(output);
+	            oo.setState(state);
+            }
+        }else if(state==CLERKLOGIN){ // TESTED
+        	o=outputHandler.clerkLogin(input);
+ 		output=o.getOutput();
+ 		state=o.getState();
+ 		oo.setOutput(output);
+            oo.setState(state);
+        }else if(state==USERLOGIN){ // TESTED
+        	o=outputHandler.userLogin(input);
+ 		output=o.getOutput();
+ 		state=o.getState();
+ 		oo.setOutput(output);
+            oo.setState(state);
+        }else if (state==CLERK){ // TESTED
+        	if (input.equalsIgnoreCase("create user")) {
+            	output = "Please Input User Info:'username,password'";
+            	state=CREATEUSER;
+            	oo.setOutput(output);
+	            oo.setState(state);
+            }else if (input.equalsIgnoreCase("create title")) { // TESTED
+            	output = "Please Input Title Info:'ISBN,title'";
+            	state=CREATETITLE;
+            	oo.setOutput(output);
+	            oo.setState(state);
+            }else if (input.equalsIgnoreCase("create item")) { // TESTED
+	            output = "Please Input Item Info:'ISBN'";
+	            state=CREATEITEM;
+	            oo.setOutput(output);
+	            oo.setState(state);
+            }else if (input.equalsIgnoreCase("delete user")) { // TESTED
+            	output = "Please Input User Info:'useremail'";
+            	state=DELETEUSER;
+            	oo.setOutput(output);
+	            oo.setState(state);
+            }else if (input.equalsIgnoreCase("delete title")) { // TESTED
+	            output = "Please Input Title Info:'ISBN'";
+	            state=DELETETITLE;
+	            oo.setOutput(output);
+	            oo.setState(state);
+            }else if (input.equalsIgnoreCase("delete item")) { // TESTED
+            	output = "Please Input Item Info:'ISBN,copynumber'";
+            	state=DELETEITEM;
+            	oo.setOutput(output);
+	            oo.setState(state);
+            }else if(input.equalsIgnoreCase("log out")){ // TESTED
+            	output = "Successfully Log Out!";
+                state = WAITING;
+                oo.setOutput(output);
+	            oo.setState(state);
+            }else if(input.equalsIgnoreCase("main menu")){ // TESTED
+        		output = "What can I do for you?Menu:Create User/Title/Item,Delete User/Title/Item.";
+                state = CLERK;
+                oo.setOutput(output);
+	            oo.setState(state);
+        	}else{ // TESTED
+            	output = "Please select from the menu.Menu:Create User/Title/Item,Delete User/Title/Item.";
+                state = CLERK;
+                oo.setOutput(output);
+	            oo.setState(state);
+            }
+        }else if (state==USER){ // TESTED
+        	if (input.equalsIgnoreCase("borrow")) {
+            	output = "Please Input User Info:'useremail,ISBN,copynumber'";
+            	state=BORROW;
+            	oo.setOutput(output);
+	            oo.setState(state);
+            }else if (input.equalsIgnoreCase("renew")) { // TESTED
+            	output = "Please Input Title Info:'useremail,ISBN,copynumber'";
+            	state=RENEW;
+            	oo.setOutput(output);
+	            oo.setState(state);
+            }else if (input.equalsIgnoreCase("return")) { // TESTED
+	            output = "Please Input Item Info:'useremail,ISBN,copynumber'";
+	            state=RETURN;
+	            oo.setOutput(output);
+	            oo.setState(state);
+            }else if (input.equalsIgnoreCase("pay fine")) { // TESTED
+            	output = "Please Input User Info:'useremail'";
+            	state=PAYFINE;
+            	oo.setOutput(output);
+	            oo.setState(state);
+            }else if(input.equalsIgnoreCase("log out")){ // TESTED
+            	output = "Successfully Log Out!";
+                state = WAITING;
+                oo.setOutput(output);
+	            oo.setState(state);
+            }else if(input.equalsIgnoreCase("main menu")){ // TESTED
+        		output = "What can I do for you?Menu:Borrow,Renew,Return,Pay Fine.";
+                state = USER;
+                oo.setOutput(output);
+	            oo.setState(state);
+        	}else{ // TESTED
+            	output = "Please select from the menu.Menu:Borrow,Renew,Return,Pay Fine.";
+                state = USER;
+                oo.setOutput(output);
+	            oo.setState(state);
+            }
+        	
+        }else if(state==CREATEUSER){
+        	if(input.equalsIgnoreCase("log out")){ // TESTED
+            	output = "Successfully Log Out!";
+                state = WAITING;
+                oo.setOutput(output);
+	            oo.setState(state);
+        	}else if(input.equalsIgnoreCase("main menu")){ // TESTED
+        		output = "What can I do for you?Menu:Create User/Title/Item,Delete User/Title/Item.";
+                state = CLERK;
+                oo.setOutput(output);
+	            oo.setState(state);
+        	}else{ // TESTED
+        		o=outputHandler.createUser(input);
+        		output=o.getOutput();
+        		state=o.getState();
+        		oo.setOutput(output);
+	            oo.setState(state);
+        	}
+        }else if(state==CREATETITLE){ // TESTED
+        	if(input.equalsIgnoreCase("log out")){
+            	output = "Successfully Log Out!";
+                state = WAITING;
+                oo.setOutput(output);
+	            oo.setState(state);
+        	}else if(input.equalsIgnoreCase("main menu")){
+        		output = "What can I do for you?Menu:Create User/Title/Item,Delete User/Title/Item.";
+                state = CLERK;
+                oo.setOutput(output);
+	            oo.setState(state);
+        	}else{
+        		o=outputHandler.createTitle(input);
+        		output=o.getOutput();
+        		state=o.getState();
+        		oo.setOutput(output);
+	            oo.setState(state);
+        	}
+        }else if(state==CREATEITEM){ // TESTED
+        	if(input.equalsIgnoreCase("log out")){
+            	output = "Successfully Log Out!";
+                state = WAITING;
+                oo.setOutput(output);
+	            oo.setState(state);
+        	}else if(input.equalsIgnoreCase("main menu")){
+        		output = "What can I do for you?Menu:Create User/Title/Item,Delete User/Title/Item.";
+                state = CLERK;
+                oo.setOutput(output);
+	            oo.setState(state);
+        	}else{
+        		o=outputHandler.createItem(input);
+        		output=o.getOutput();
+        		state=o.getState();
+        		oo.setOutput(output);
+	            oo.setState(state);
+        	}
+        }else if(state==DELETEUSER){ // TESTED
+        	if(input.equalsIgnoreCase("log out")){
+            	output = "Successfully Log Out!";
+                state = WAITING;
+                oo.setOutput(output);
+	            oo.setState(state);
+        	}else if(input.equalsIgnoreCase("main menu")){
+        		output = "What can I do for you?Menu:Create User/Title/Item,Delete User/Title/Item.";
+                state = CLERK;
+                oo.setOutput(output);
+	            oo.setState(state);
+        	}else{
+        		o=outputHandler.deleteUser(input);
+        		output=o.getOutput();
+        		state=o.getState();
+        		oo.setOutput(output);
+	            oo.setState(state);
+        	}
+        }else if(state==DELETETITLE){ // TESTED
+        	if(input.equalsIgnoreCase("log out")){
+            	output = "Successfully Log Out!";
+                state = WAITING;
+                oo.setOutput(output);
+	            oo.setState(state);
+        	}else if(input.equalsIgnoreCase("main menu")){
+        		output = "What can I do for you?Menu:Create User/Title/Item,Delete User/Title/Item.";
+                state = CLERK;
+                oo.setOutput(output);
+	            oo.setState(state);
+        	}else{
+        		o=outputHandler.deleteTitle(input);
+        		output=o.getOutput();
+        		state=o.getState();
+        		oo.setOutput(output);
+	            oo.setState(state);
+        	}
+        }else if(state==DELETEITEM){ // TESTED
+        	if(input.equalsIgnoreCase("log out")){
+            	output = "Successfully Log Out!";
+                state = WAITING;
+                oo.setOutput(output);
+	            oo.setState(state);
+        	}else if(input.equalsIgnoreCase("main menu")){
+        		output = "What can I do for you?Menu:Create User/Title/Item,Delete User/Title/Item.";
+                state = CLERK;
+                oo.setOutput(output);
+	            oo.setState(state);
+        	}else{
+        		o=outputHandler.deleteItem(input);
+        		output=o.getOutput();
+        		state=o.getState();
+        		oo.setOutput(output);
+	            oo.setState(state);
+        	}
+        }else if(state==BORROW){ // TESTED
+        	if(input.equalsIgnoreCase("log out")){
+            	output = "Successfully Log Out!";
+                state = WAITING;
+                oo.setOutput(output);
+	            oo.setState(state);
+        	}else if(input.equalsIgnoreCase("main menu")){
+        		output = "What can I do for you?Menu:Borrow,Renew,Return,Pay Fine.";
+                state = USER;
+                oo.setOutput(output);
+	            oo.setState(state);
+        	}else{
+        		o=outputHandler.borrow(input);
+        		output=o.getOutput();
+        		state=o.getState();
+        		oo.setOutput(output);
+	            oo.setState(state);
+        	}
+        }else if(state==RENEW){ // TESTED
+        	if(input.equalsIgnoreCase("log out")){
+            	output = "Successfully Log Out!";
+                state = WAITING;
+                oo.setOutput(output);
+	            oo.setState(state);
+        	}else if(input.equalsIgnoreCase("main menu")){
+        		output = "What can I do for you?Menu:Borrow,Renew,Return,Pay Fine.";
+                state = USER;
+                oo.setOutput(output);
+	            oo.setState(state);
+        	}else{
+        		o=outputHandler.renew(input);
+        		output=o.getOutput();
+        		state=o.getState();
+        		oo.setOutput(output);
+	            oo.setState(state);
+        	}
+        }else if(state==RETURN){ // TESTED
+        	if(input.equalsIgnoreCase("log out")){
+            	output = "Successfully Log Out!";
+                state = WAITING;
+                oo.setOutput(output);
+	            oo.setState(state);
+        	}else if(input.equalsIgnoreCase("main menu")){
+        		output = "What can I do for you?Menu:Borrow,Renew,Return,Pay Fine.";
+                state = USER;
+                oo.setOutput(output);
+	            oo.setState(state);
+        	}else{
+        		o=outputHandler.returnBook(input);
+        		output=o.getOutput();
+        		state=o.getState();
+        		oo.setOutput(output);
+	            oo.setState(state);
+        	}
+        }else if(state==PAYFINE){ // 
+        	if(input.equalsIgnoreCase("log out")){
+            	output = "Successfully Log Out!";
+                state = WAITING;
+                oo.setOutput(output);
+	            oo.setState(state);
+        	}else if(input.equalsIgnoreCase("main menu")){
+        		output = "What can I do for you?Menu:Borrow,Renew,Return,Pay Fine.";
+                state = USER;
+                oo.setOutput(output);
+	            oo.setState(state);
+        	}else{
+        		o=outputHandler.payFine(input);
+        		output=o.getOutput();
+        		state=o.getState();
+        		oo.setOutput(output);
+	            oo.setState(state);
+        	}
+		 }
 	     return oo;
 	}
 }
--- src/server/logic/handler/tests/InputHandlerTest.java
+++ src/server/logic/handler/tests/InputHandlerTest.java
@@ -2,6 +2,7 @@ package server.logic.handler.tests;
 
 import static org.junit.Assert.*;
 import server.logic.handler.InputHandler;
+import server.logic.handler.OutputHandler;
 import server.logic.handler.model.Output;
 import server.logic.handler.model.ServerOutput;
 
@@ -10,16 +11,193 @@ import org.junit.Test;
 public class InputHandlerTest {
 
 	private InputHandler testInputHandler = new InputHandler();
+	private OutputHandler testOutputHandler = new OutputHandler();
+	
+	public int WAITING = 0;
+	public int FINISHWAITING=1;
+	public int CLERK = 2;
+    public int USER = 3;
+    public int CREATEUSER=4;
+    public int CREATETITLE=5;
+    public int CREATEITEM=6;
+    public int DELETEUSER=7;
+    public int DELETETITLE=8;
+    public int DELETEITEM=9;
+    public int BORROW=10;
+    public int RENEW=11;
+    public int RETURN=12;
+    public int PAYFINE=13;
+    public int CLERKLOGIN=14;
+    public int USERLOGIN=15;
+    
+	private ServerOutput testServerOutput = new ServerOutput("",0);
 	
 	@Test
 	public void test_processInput() {
-		//Output testOutput = new Output("", 0);
-		//ServerOutput testServerOutput = new ServerOutput("", 0);
 		
-		assertEquals("Who Are you?Clerk or User?", testInputHandler.processInput("", 0).getOutput());
-		assertEquals(0, testInputHandler.processInput("", 0).getState());
+		testServerOutput = testInputHandler.processInput("", WAITING);
+		assertEquals("Who Are you?Clerk or User?", testServerOutput.getOutput());
+		assertEquals(FINISHWAITING, testServerOutput.getState());
 		
+		testServerOutput = testInputHandler.processInput("clerk", FINISHWAITING);
+		assertEquals("Please Input The Password:", testServerOutput.getOutput());
+		assertEquals(CLERKLOGIN, testServerOutput.getState());
 		
+		testServerOutput = testInputHandler.processInput("user", FINISHWAITING);
+		assertEquals("Please Input Username and Password:'username,password'", testServerOutput.getOutput());
+		assertEquals(USERLOGIN, testServerOutput.getState());
+		
+		testServerOutput = testInputHandler.processInput("somethingelse", FINISHWAITING);
+		assertEquals("Who Are you?Clerk or User?", testServerOutput.getOutput());
+		assertEquals(FINISHWAITING, testServerOutput.getState());
+		
+		testServerOutput = testInputHandler.processInput("admin", CLERKLOGIN);
+		assertEquals("What can I do for you?Menu:Create User/Title/Item,Delete User/Title/Item.", testServerOutput.getOutput());
+		assertEquals(CLERK, testServerOutput.getState());
+		
+		testServerOutput = testInputHandler.processInput("michelle@carleton.ca,Michelle", USERLOGIN);
+		assertEquals("What can I do for you?Menu:Borrow,Renew,Return,Pay Fine.", testServerOutput.getOutput());
+		assertEquals(USER, testServerOutput.getState());
+		
+		testServerOutput = testInputHandler.processInput("create user", CLERK);
+		assertEquals("Please Input User Info:'username,password'", testServerOutput.getOutput());
+		assertEquals(CREATEUSER, testServerOutput.getState());
+		
+		testServerOutput = testInputHandler.processInput("create title", CLERK);
+		assertEquals("Please Input Title Info:'ISBN,title'", testServerOutput.getOutput());
+		assertEquals(CREATETITLE, testServerOutput.getState());
+		
+		testServerOutput = testInputHandler.processInput("create item", CLERK);
+		assertEquals("Please Input Item Info:'ISBN'", testServerOutput.getOutput());
+		assertEquals(CREATEITEM, testServerOutput.getState());
+		
+		testServerOutput = testInputHandler.processInput("delete user", CLERK);
+		assertEquals("Please Input User Info:'useremail'", testServerOutput.getOutput());
+		assertEquals(DELETEUSER, testServerOutput.getState());
+		
+		testServerOutput = testInputHandler.processInput("delete title", CLERK);
+		assertEquals("Please Input Title Info:'ISBN'", testServerOutput.getOutput());
+		assertEquals(DELETETITLE, testServerOutput.getState());
+		
+		testServerOutput = testInputHandler.processInput("delete item", CLERK);
+		assertEquals("Please Input Item Info:'ISBN,copynumber'", testServerOutput.getOutput());
+		assertEquals(DELETEITEM, testServerOutput.getState());
+		
+		testServerOutput = testInputHandler.processInput("log out", CLERK);
+		assertEquals("Successfully Log Out!", testServerOutput.getOutput());
+		assertEquals(WAITING, testServerOutput.getState());
+		
+		testServerOutput = testInputHandler.processInput("main menu", CLERK);
+		assertEquals("What can I do for you?Menu:Create User/Title/Item,Delete User/Title/Item.", testServerOutput.getOutput());
+		assertEquals(CLERK, testServerOutput.getState());
+		
+		testServerOutput = testInputHandler.processInput("borrow", USER);
+		assertEquals("Please Input User Info:'useremail,ISBN,copynumber'", testServerOutput.getOutput());
+		assertEquals(BORROW, testServerOutput.getState());
+		
+		testServerOutput = testInputHandler.processInput("renew", USER);
+		assertEquals("Please Input Title Info:'useremail,ISBN,copynumber'", testServerOutput.getOutput());
+		assertEquals(RENEW, testServerOutput.getState());
+		
+		testServerOutput = testInputHandler.processInput("return", USER);
+		assertEquals("Please Input Item Info:'useremail,ISBN,copynumber'", testServerOutput.getOutput());
+		assertEquals(RETURN, testServerOutput.getState());
+		
+		testServerOutput = testInputHandler.processInput("pay fine", USER);
+		assertEquals("Please Input User Info:'useremail'", testServerOutput.getOutput());
+		assertEquals(PAYFINE, testServerOutput.getState());
+		
+		testServerOutput = testInputHandler.processInput("log out", USER);
+		assertEquals("Successfully Log Out!", testServerOutput.getOutput());
+		assertEquals(WAITING, testServerOutput.getState());
+		
+		testServerOutput = testInputHandler.processInput("main menu", USER);
+		assertEquals("What can I do for you?Menu:Borrow,Renew,Return,Pay Fine.", testServerOutput.getOutput());
+		assertEquals(USER, testServerOutput.getState());
+		
+		testServerOutput = testInputHandler.processInput("somethingelse", USER);
+		assertEquals("Please select from the menu.Menu:Borrow,Renew,Return,Pay Fine.", testServerOutput.getOutput());
+		assertEquals(USER, testServerOutput.getState());
+		
+		testServerOutput = testInputHandler.processInput("log out", CREATEUSER);
+		assertEquals("Successfully Log Out!", testServerOutput.getOutput());
+		assertEquals(WAITING, testServerOutput.getState());
+		
+		testServerOutput = testInputHandler.processInput("main menu", CREATEUSER);
+		assertEquals("What can I do for you?Menu:Create User/Title/Item,Delete User/Title/Item.", testServerOutput.getOutput());
+		assertEquals(CLERK, testServerOutput.getState());
+		
+		testServerOutput = testInputHandler.processInput("log out", CREATETITLE);
+		assertEquals("Successfully Log Out!", testServerOutput.getOutput());
+		assertEquals(WAITING, testServerOutput.getState());
+		
+		testServerOutput = testInputHandler.processInput("main menu", CREATETITLE);
+		assertEquals("What can I do for you?Menu:Create User/Title/Item,Delete User/Title/Item.", testServerOutput.getOutput());
+		assertEquals(CLERK, testServerOutput.getState());
+		
+		testServerOutput = testInputHandler.processInput("log out", CREATEITEM);
+		assertEquals("Successfully Log Out!", testServerOutput.getOutput());
+		assertEquals(WAITING, testServerOutput.getState());
+		
+		testServerOutput = testInputHandler.processInput("main menu", CREATEITEM);
+		assertEquals("What can I do for you?Menu:Create User/Title/Item,Delete User/Title/Item.", testServerOutput.getOutput());
+		assertEquals(CLERK, testServerOutput.getState());
+		
+		testServerOutput = testInputHandler.processInput("log out", DELETEUSER);
+		assertEquals("Successfully Log Out!", testServerOutput.getOutput());
+		assertEquals(WAITING, testServerOutput.getState());
+		
+		testServerOutput = testInputHandler.processInput("main menu", DELETEUSER);
+		assertEquals("What can I do for you?Menu:Create User/Title/Item,Delete User/Title/Item.", testServerOutput.getOutput());
+		assertEquals(CLERK, testServerOutput.getState());
+		
+		testServerOutput = testInputHandler.processInput("log out", DELETETITLE);
+		assertEquals("Successfully Log Out!", testServerOutput.getOutput());
+		assertEquals(WAITING, testServerOutput.getState());
+		
+		testServerOutput = testInputHandler.processInput("main menu", DELETETITLE);
+		assertEquals("What can I do for you?Menu:Create User/Title/Item,Delete User/Title/Item.", testServerOutput.getOutput());
+		assertEquals(CLERK, testServerOutput.getState());
+		
+		testServerOutput = testInputHandler.processInput("log out", DELETEITEM);
+		assertEquals("Successfully Log Out!", testServerOutput.getOutput());
+		assertEquals(WAITING, testServerOutput.getState());
+		
+		testServerOutput = testInputHandler.processInput("main menu", DELETEITEM);
+		assertEquals("What can I do for you?Menu:Create User/Title/Item,Delete User/Title/Item.", testServerOutput.getOutput());
+		assertEquals(CLERK, testServerOutput.getState());
+		
+		testServerOutput = testInputHandler.processInput("log out", BORROW);
+		assertEquals("Successfully Log Out!", testServerOutput.getOutput());
+		assertEquals(WAITING, testServerOutput.getState());
+		
+		testServerOutput = testInputHandler.processInput("main menu", BORROW);
+		assertEquals("What can I do for you?Menu:Borrow,Renew,Return,Pay Fine.", testServerOutput.getOutput());
+		assertEquals(USER, testServerOutput.getState());
+		
+		testServerOutput = testInputHandler.processInput("log out", RENEW);
+		assertEquals("Successfully Log Out!", testServerOutput.getOutput());
+		assertEquals(WAITING, testServerOutput.getState());
+		
+		testServerOutput = testInputHandler.processInput("main menu", RENEW);
+		assertEquals("What can I do for you?Menu:Borrow,Renew,Return,Pay Fine.", testServerOutput.getOutput());
+		assertEquals(USER, testServerOutput.getState());
+		
+		testServerOutput = testInputHandler.processInput("log out", RETURN);
+		assertEquals("Successfully Log Out!", testServerOutput.getOutput());
+		assertEquals(WAITING, testServerOutput.getState());
+		
+		testServerOutput = testInputHandler.processInput("main menu", RETURN);
+		assertEquals("What can I do for you?Menu:Borrow,Renew,Return,Pay Fine.", testServerOutput.getOutput());
+		assertEquals(USER, testServerOutput.getState());
+		
+		testServerOutput = testInputHandler.processInput("log out", PAYFINE);
+		assertEquals("Successfully Log Out!", testServerOutput.getOutput());
+		assertEquals(WAITING, testServerOutput.getState());
+		
+		testServerOutput = testInputHandler.processInput("main menu", PAYFINE);
+		assertEquals("What can I do for you?Menu:Borrow,Renew,Return,Pay Fine.", testServerOutput.getOutput());
+		assertEquals(USER, testServerOutput.getState());
 	}
 
 }
