214b5de37e65f5f0fc12df054507ccd870ffb334
theoJA <theophilus.ja@gmail.com>
2017-10-10 11:31:02 -0400

    CLASS: OutputHandler, TEST: createTitle SUCCESS

--- src/server/logic/handler/tests/OutputHandlerTest.java
+++ src/server/logic/handler/tests/OutputHandlerTest.java
@@ -6,6 +6,10 @@ import org.junit.Test;
 
 import server.logic.handler.OutputHandler;
 import server.logic.tables.UserTable;
+import server.logic.tables.TitleTable;
+import server.logic.tables.ItemTable;
+import server.logic.tables.FeeTable;
+import server.logic.tables.LoanTable;
 
 public class OutputHandlerTest {
 
@@ -24,8 +28,15 @@ public class OutputHandlerTest {
 		
 	}
 	
-	//@Test
-	//public void test_createTitle
+	@Test
+	public void test_createTitle() {
+		assertEquals("Your input should in this format:'ISBN,title',ISBN should be a 13-digit number", testOutputHandler.createTitle("wrongformat,wrongformat").getOutput());
+		assertEquals("The Title Already Exists!", testOutputHandler.createTitle("9781442668584,By the grace of God").getOutput());
+		assertEquals("Success!", testOutputHandler.createTitle("9781442668585,By the grace of Not God").getOutput());
+		
+		TitleTable testTitleTable = TitleTable.getInstance();
+		testTitleTable.delete("9781442668585");
+	}
 	
 
 }
